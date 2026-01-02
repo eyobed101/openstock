06b0a547f48c0d2e4b7679669ee7ec3a83adc4be
eyobed101 <eyocjjazz101@gmail.com>
2026-01-02 21:51:46 +0300

    settings

--- app/pages/settings/index.vue
+++ app/pages/settings/index.vue
@@ -332,7 +332,7 @@ const ui = {
           <div class="bg-gray-50 px-4 py-3 border-t border-gray-100">
             <p class="text-xs text-gray-500 text-center">
               Alerts are sent to
-              <span class="font-medium text-gray-900">admin@openstock.io</span>
+              <span class="font-medium text-gray-900">admin@gmail.com</span>
             </p>
           </div>
         </div>
